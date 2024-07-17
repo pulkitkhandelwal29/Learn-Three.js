827127998e6511ef0056fe839bb0b4fccfb1cb22
Pulkit Khandelwal <67990422+pulkitkhandelwal29@users.noreply.github.com>
2024-07-17 20:54:45 +0530

    OrbitControls & Renderloop

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 //importing three js module using cdn
 import * as THREE from 'three';
 
+//importing OrbitControls (addon)
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
 //initialize the scene
-const scene = new THREE.Scene();
+const scene = new THREE.Scene()
 
 ////add objects to the scene
 
@@ -17,33 +20,50 @@ const cubeMesh = new THREE.Mesh(
 )
 
 //mesh added to the scene in a parent-children relationship
-scene.add(cubeMesh);
+scene.add(cubeMesh)
 
 
 
 //initialize the camera (Field of View, aspect ratio, near, far)
 const camera = new THREE.PerspectiveCamera( 50,
      window.innerWidth / window.innerHeight,
-     0.1, 30);
+     0.1, 30)
 
 //camera is positioned this units away
-camera.position.z = 5;
+camera.position.z = 5
 
-scene.add(camera);
+scene.add(camera)
 
 
 ////initialize the renderer
 
 //referencing the threejs canvas defined in html
-const canvas = document.querySelector('canvas.threejs');
+const canvas = document.querySelector('canvas.threejs')
 
 //creating a renderer which takes parameter as canvas
 const renderer = new THREE.WebGLRenderer({
     canvas:canvas
-});
+})
 
 //setting the renderer size to the window size
-renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setSize( window.innerWidth, window.innerHeight )
 
-//running the renderer
-renderer.render(scene,camera);
+//instantiate the controls (takes camera and dom selector as the parameters)
+const controls = new OrbitControls(camera,canvas)
+
+//for smooth rotation and maintained figure
+controls.enableDamping = true
+controls.autoRotate = true
+
+//normal rendering only takes a frame and render while if we want to make it 3D, we need to use the concept of render loop
+const renderloop = () => {
+    //updates the control accordingly
+    controls.update()
+    //running the renderer after it is updated
+    renderer.render(scene,camera)
+    //requestAnimationFrame allows renderloop only when it is required (unlike infinite loop)
+    window.requestAnimationFrame(renderloop)
+}
+
+renderloop()
+
